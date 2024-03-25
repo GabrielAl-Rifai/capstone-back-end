0df3bf4644215db19bec66d45d286993ee6839c1
Gabriel Al-Rifai <Gabrielalrifai@gmail.com>
2024-03-25 16:35:47 -0500

    updated routes

--- routes/api/days.js
+++ routes/api/days.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Days = require("../../models/DaysSchema");
 
-app.get('http://localhost:3000/api/days/seed', async (req, res) => {
+app.get('/api/days/seed', async (req, res) => {
   await Days.deleteMany({});
   await Days.create(Days);
 
@@ -10,7 +10,7 @@ app.get('http://localhost:3000/api/days/seed', async (req, res) => {
 });
 
 //Create
-app.post('http://localhost:3000/api/days', async (req, res) => {
+app.post('/api/days', async (req, res) => {
   try {
     let newDay = new Days(req.body);
     await newDays.save();
@@ -22,43 +22,43 @@ app.post('http://localhost:3000/api/days', async (req, res) => {
   }
 });
 
-// //Read
-// app.get('http://localhost:3000/api/days', async (req, res) => {
-//   try {
-//     const allDays = await Days.find({});
-//     res.json(allDays);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ msg: 'Server Error' });
-//   }
-// });
+//Read
+app.get('/api/days', async (req, res) => {
+  try {
+    const allDays = await Days.find({});
+    res.json(allDays);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
 
-// //Update
-// app.put('http://localhost:3000/api/days:id', async (req, res) => {
-//   try {
-//     const updatedDay = await Days.findByIdAndUpdate(
-//       req.params.id,
-//       req.body,
-//       { new: true }
-//     );
+//Update
+app.put('/api/days:id', async (req, res) => {
+  try {
+    const updatedDay = await Days.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
 
-//     res.json(updatedDay);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ msg: 'Server Error' });
-//   }
-// });
+    res.json(updatedDay);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
 
-// //Delete
-// app.delete('http://localhost:3000/api/days:id', async (req, res) => {
-//   try {
-//     await Days.findByIdAndDelete(req.params.id);
+//Delete
+app.delete('/api/days:id', async (req, res) => {
+  try {
+    await Days.findByIdAndDelete(req.params.id);
 
-//     res.status(200).json({ msg: 'Item Deleted' });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ msg: 'Server Error' });
-//   }
-// });
+    res.status(200).json({ msg: 'Item Deleted' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
 
-// module.exports = router;
+module.exports = router;
--- routes/api/meals.js
+++ routes/api/meals.js
@@ -1,75 +1,72 @@
-// const express = require("express");
-// const router = express.Router();
-// const Meals = require("../../models/MealsSchema");
+const express = require("express");
+const router = express.Router();
+const Meals = require("../../models/MealsSchema");
 
 
 
+router.get("/seed", async (req, res) => {
+  await Meals.deleteMany({});
+  await Meals.create(Meals);
 
-//Get a list of 25 recipes
+  res.send(`Database Seeded`);
+});
 
-// router.get("/seed", async (req, res) => {
-//   await Meals.deleteMany({});
-//   await Meals.create(Meals);
-
-//   res.send(`Database Seeded`);
-// });
-
-// // router.post("/",[])
+// router.post("/",[])
 
 
-// //Create
-// router.post("http://localhost:3000/api/meals", async (req, res) => {
-//   try {
-//     let newMeal = new Meals(req.body);
-//     await newMeal.save();
+//Create
+router.post('/api/meals', async (req, res) => {
+  try {
+    let newMeal = new Meals(req.body);
+    await newMeal.save();
 
-//     res.json(newMeal);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ msg: "Server Error" });
-//   }
-// });
+    res.json(newMeal);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
 
-// //Read
-// router.get("http://localhost:3000/api/meals", async (req, res) => {
-//   try {
-//     const allMeals = await Meals.find({});
-//     res.json(allMeals);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ msg: "Server Error" });
-//   }
-// });
+//Read
+router.get('/api/meals', async (req, res) => {
+  try {
+    const allMeals = await Meals.find({});
+    res.json(allMeals);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
 
-// //Update
-// router.put("http://localhost:3000/api/meals:id", async (req, res) => {
-//   try {
-//     const updatedMeal = await Meals.findByIdAndUpdate(req.params.id, req.body, {
-//       new: true,
-//     });
+//Update
+router.put('/api/meals', async (req, res) => {
+  try {
+    const updatedMeal = await Meals.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
 
-//     res.json(updatedMeal);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ msg: "Server Error" });
-//   }
-// });
+    res.json(updatedMeal);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
 
-// //Delete
-// app.delete("http://localhost:3000/api/meals:id", async (req, res) => {
-//   try {
-//     await Meals.findByIdAndDelete(req.params.id);
+//Delete
+app.delete('/api/meals:id', async (req, res) => {
+  try {
+    await Meals.findByIdAndDelete(req.params.id);
 
-//     res.status(200).json({ msg: "Item Deleted" });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ msg: "Server Error" });
-//   }
-// });
+    res.status(200).json({ msg: "Item Deleted" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
 
-// //Error checking middleware
-// app.use((err, _req, res, next) => {
-//   res.status(500).send("Seems like we messed up somewhere...");
-// });
+//Error checking middleware
+app.use((err, _req, res, next) => {
+  res.status(500).send("Seems like we messed up somewhere...");
+});
 
-// module.exports = router;
+module.exports = router;
